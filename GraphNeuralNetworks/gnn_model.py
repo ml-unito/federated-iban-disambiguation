b2c0257182767e56cf7225cc43aef6bf391f0cc5
giulia-luisa-fumiento <giuliafumiento@gmail.com>
2024-07-14 18:51:04 +0200

    Create GNN model with GAT layers.

---
+++ GraphNeuralNetworks/gnn_model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+
+
+class GNN(torch.nn.Module):
+	def __init__(self, input_dim):
+		super().__init__()
+		self.gatconv1 = GATConv(in_channels=input_dim, out_channels=input_dim)
+		self.gatconv2 = GATConv(in_channels=input_dim, out_channels=input_dim)
+
+	def forward(self, graph):
+		x = graph.x
+		edge_index = graph.edge_index
+
+		x = self.gatconv1(x, edge_index)
+		x = F.leaky_relu(x, negative_slope=0.02)
+		x = F.dropout(x, p=0.6, training=self.training)
+		x = self.gatconv2(x, edge_index)
+
+		xT = x.t()
+		pred = x @ xT
+
+		# print("xT",xT.size())
+		# print(pred)
+		# print(pred.size())
+		return pred
