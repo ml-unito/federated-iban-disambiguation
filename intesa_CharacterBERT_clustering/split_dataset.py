ad2bb52ccb7e2d41237a0086a4c98589b40e67d2
Giulia Luisa Fumiento <giuliafumiento@gmail.com>
2025-04-04 18:31:17 +0200

    Edited script to split dataset.

--- intesa_CharacterBERT_clustering/split_dataset.py
+++ intesa_CharacterBERT_clustering/split_dataset.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from lib.datasetManipulation import *
-import sys
 
 DIR_PATH = "./dataset/"
 DATASET_PATH = "benchmark_intesa.csv"
@@ -11,12 +10,22 @@ SAVE_DATASETS = True
 
 def test_datasets(datasets: list, prob_shared: float):
   for dataset in datasets:
-    current_prob_shared = dataset.groupby('IsShared').size()[1] * 100 / len(dataset)
-    if not (current_prob_shared >= (prob_shared - 5) and current_prob_shared <= (prob_shared + 5)):
+    current_prob_shared = dataset[dataset['IsShared'] == 1]['AccountNumber'].nunique() / len(set(dataset["AccountNumber"])) * 100 #dataset.groupby('IsShared').size()[1] * 100 / len(dataset)
+    if not (current_prob_shared >= (prob_shared - 3) and current_prob_shared <= (prob_shared + 3)):
       return 0
   
   return 1
 
+
+def split_dataset(dataset: pd.DataFrame, train_size: float, random_state=42):
+  iban_list = dataset.AccountNumber.unique()
+  isShared = dataset.groupby('AccountNumber', sort=False)['IsShared'].first().loc[iban_list].values
+  x_iban_list, y_iban_list = train_test_split(iban_list, train_size=train_size, stratify=isShared)#, random_state=random_state)
+  
+  df_x = dataset.loc[dataset.AccountNumber.isin(x_iban_list)]
+  df_y = dataset.loc[dataset.AccountNumber.isin(y_iban_list)]
+
+  return df_x, df_y
 
 
 def create_couple_dataset(dataset: str, output_path: str, to_save=False):
@@ -30,61 +39,49 @@ def create_couple_dataset(dataset: str, output_path: str, to_save=False):
     return dataset
 
 
-
-def split_dataset(dataset: pd.DataFrame, train_size: float):
-  iban_list = dataset.AccountNumber.unique()
-  isShared = dataset.groupby('AccountNumber', sort=False)['IsShared'].first().loc[iban_list].values
-  x_iban_list, y_iban_list = train_test_split(iban_list, train_size=train_size, stratify=isShared)
-  
-  df_x = dataset.loc[dataset.AccountNumber.isin(x_iban_list)]
-  df_y = dataset.loc[dataset.AccountNumber.isin(y_iban_list)]
-
-  return df_x, df_y
+def get_shared_prob(df):
+  return round(df[df['IsShared'] == 1]['AccountNumber'].nunique() / len(set(df["AccountNumber"])) * 100, 2)
 
 
-
-def generate_datasets():
-  dataset = pd.read_csv(DIR_PATH + DATASET_PATH, index_col=0)
-  dataset["Name"] = dataset["Name"].str.lower()
-  dataset["Holder"] = dataset["Holder"].str.lower()
-
+def create_train_test_split(dataset):
   generating = True
   num_generation = 0
+
   print("Generating datasets ...")
-  while generating or num_generation <= 10:
+  while generating and num_generation <= 10:
     num_generation += 1
 
-    df_train_fl, df_test_clustering = split_dataset(dataset, train_size=0.8)
-    df_clients, df_server = split_dataset(df_train_fl, train_size=0.8)
+    # Split dataset for train and test (80-20)
+    df_train, df_test = split_dataset(dataset, train_size=0.8)
 
     # Split dataset between four client, with ratio 40-40-10-10
-    x,y = split_dataset(df_clients, 0.8)
+    x,y = split_dataset(df_train, 0.8)
     df_client1, df_client2 = split_dataset(x, 0.5)
     df_client3, df_client4 = split_dataset(y, 0.5)
-    
-    # Test datasets generated: if they not respects shared proportion, the new datasets will generate.
-    is_correct = test_datasets(datasets=[df_client1, df_client2, df_client3, df_client4, df_server, df_test_clustering],
-                  prob_shared=dataset.groupby('IsShared').size()[1]*100/len(dataset))
 
+    # Test datasets generated: if they not respects shared proportion, the new datasets will generate.
+    is_correct = test_datasets(datasets=[df_test, df_train, df_client1, df_client2, df_client3, df_client4],
+                  prob_shared=get_shared_prob(dataset))
+    
     if is_correct:
       generating = False
-      print(f"\nclient1:\tlen {len(df_client1)}\t prob shared {round(df_client1.groupby('IsShared').size()[1] * 100 / len(df_client1), 2)}")
-      print(f"client2:\tlen {len(df_client2)}\t prob shared {round(df_client2.groupby('IsShared').size()[1] * 100 / len(df_client2),2)}")
-      print(f"client3:\tlen {len(df_client3)}\t\t prob shared {round(df_client3.groupby('IsShared').size()[1] * 100 / len(df_client3), 2)}")
-      print(f"client4:\tlen {len(df_client4)}\t\t prob shared {round(df_client4.groupby('IsShared').size()[1] * 100 / len(df_client4), 2)}")
-      print(f"server:\tlen {len(df_server)}\t\t\t prob shared {round(df_server.groupby('IsShared').size()[1] * 100 / len(df_server), 2)}")
-      print(f"clustering:\tlen {len(df_server)}\t\t prob shared {round(df_server.groupby('IsShared').size()[1] * 100 / len(df_server), 2)}\n")
+      print(f"Original dataset:\tlen {len(dataset)}\t\t\t prob shared {get_shared_prob(dataset)}")
+      print(f"\nclient1:\tlen {len(df_client1)}\t prob shared {get_shared_prob(df_client1)}")
+      print(f"client2:\tlen {len(df_client2)}\t prob shared {get_shared_prob(df_client2)}")
+      print(f"client3:\tlen {len(df_client3)}\t\t prob shared {get_shared_prob(df_client3)}")
+      print(f"client4:\tlen {len(df_client4)}\t\t prob shared {get_shared_prob(df_client4)}")
+      print(f"test:\t\tlen {len(df_test)}\t\t prob shared {get_shared_prob(df_test)}")
+      print(f"train:\t\tlen {len(df_train)}\t\t prob shared {get_shared_prob(df_train)}")
       
       # Save all dataset generated
       if SAVE_DATASETS:
-        df_client1.to_csv(DIR_PATH + "client1_train.csv")
-        df_client2.to_csv(DIR_PATH + "client2_train.csv")
-        df_client3.to_csv(DIR_PATH + "client3_train.csv")
-        df_client4.to_csv(DIR_PATH + "client4_train.csv")
-        df_server.to_csv(DIR_PATH + "server_test.csv")
-        df_test_clustering.to_csv(DIR_PATH + "test_clustering.csv")
-        print("Datasets saved into " + DIR_PATH + " directory.")
-
+        df_client1.to_csv(DIR_PATH + "split_dataset/client1_train.csv")
+        df_client2.to_csv(DIR_PATH + "split_dataset/client2_train.csv")
+        df_client3.to_csv(DIR_PATH + "split_dataset/client3_train.csv")
+        df_client4.to_csv(DIR_PATH + "split_dataset/client4_train.csv")
+        df_test.to_csv(DIR_PATH + "split_dataset/df_test.csv")
+        df_train.to_csv(DIR_PATH + "split_dataset/df_train.csv")
+        print("Datasets saved into " + DIR_PATH + "split_dataset/ directory.")
 
 
 def generate_couple_datasets():
@@ -93,7 +90,8 @@ def generate_couple_datasets():
   df_client2 = pd.read_csv(DIR_PATH + "client2_train.csv", index_col=0)
   df_client3 = pd.read_csv(DIR_PATH + "client3_train.csv", index_col=0)
   df_client4 = pd.read_csv(DIR_PATH + "client4_train.csv", index_col=0)
-  df_server = pd.read_csv(DIR_PATH + "server_test.csv", index_col=0)
+  df_test = pd.read_csv(DIR_PATH + "df_test.csv", index_col=0)
+  df_train = pd.read_csv(DIR_PATH + "df_train.csv", index_col=0)
 
   print("Creating couple datasets ...")
 
@@ -101,23 +99,19 @@ def generate_couple_datasets():
   create_couple_dataset(df_client2, DIR_PATH + "client2_train_couple.csv")
   create_couple_dataset(df_client3, DIR_PATH + "client3_train_couple.csv")
   create_couple_dataset(df_client4, DIR_PATH + "client4_train_couple.csv")
-  create_couple_dataset(df_server, DIR_PATH + "server_test_couple.csv")
+  create_couple_dataset(df_test, DIR_PATH + "test_couple.csv")
+  create_couple_dataset(df_train, DIR_PATH + "train_couple.csv")
   
   print("Couple datasets is created.")
 
 
 
-def main(action: str):
-  if action == "GENERATE_DATASETS":
-    generate_datasets()
-  elif action == "GENERATE_COUPLE_DATASETS":
-    generate_couple_datasets()
+def main():
+  dataset = pd.read_csv(DIR_PATH + DATASET_PATH, index_col=0)
+
+  create_train_test_split(dataset)
 
   
 
 if __name__ == "__main__":
-  if len(sys.argv) >= 1:
-    action = sys.argv[1]
-    main(action)
-  else:
-    print("error parameters")
+  main()
