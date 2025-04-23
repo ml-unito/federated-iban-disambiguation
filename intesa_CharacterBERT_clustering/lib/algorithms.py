19bd4eb9e20afa88f1c922893c6bfcbf0dcf7c51
Roberto Esposito <roberto.esposito@unito.it>
2025-04-23 22:18:04 +0200

    Started implementation of the algorithms -- the fluke's way

---
+++ intesa_CharacterBERT_clustering/lib/algorithms.py
@@ -0,0 +1,52 @@
+from fluke.client import Client
+from fluke.server import Server
+from fluke.algorithms import PersonalizedFL
+from fluke.algorithms.lg_fedavg import LGFedAVGClient
+from torch.nn import Module
+from fluke.data import FastDataLoader
+from typing import Iterable
+
+
+# PretrainedBert
+class PretrainedBertClient(LGFedAVGClient):
+    def fit(self, override_local_epochs = 0):
+        if self._last_round == 0:
+            for param in self.model.get_local().parameters():
+                param.requires_grad = False 
+
+        return super().fit(override_local_epochs)
+
+
+class PretrainedBert(PersonalizedFL):
+    def get_client_class(self):
+        return PretrainedBertClient
+
+# FrozenBert
+
+class FrozenBertClient(Client):
+    pass
+
+class FrozenBertServer(Server):
+    pass
+
+class FrozenBert(PersonalizedFL):
+    def get_client_class(self):
+        return FrozenBertClient
+    
+    def get_server_class(self):
+        return FrozenBertServer
+
+# LocalBert
+
+class LocalBertClient(Client):
+    pass
+
+class LocalBertServer(Server):
+    pass
+
+class LocalBert(PersonalizedFL):
+    def get_client_class(self):
+        return LocalBertClient
+    
+    def get_server_class(self):
+        return LocalBertServer
---
+++ intesa_CharacterBERT_clustering/lib/models.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+from fluke import nets
+from character_bert_model.modeling.character_bert import CharacterBertModel
+
+
+class NERClassifier_D(nn.Module):
+    def __init__(self):
+        super(NERClassifier_D, self).__init__()
+
+        num_labels = 2
+        self.dropout = nn.Dropout(0.2)
+        self.hidden = nn.Linear(768, 100)
+        self.classifier = nn.Linear(100, num_labels)
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+
+    def forward(self, outputs):
+        pooled_output = self.dropout(
+            outputs[:, 0, :]
+        )  # Take the first token's embedding ([CLS])
+
+        hidden_logits = self.relu(self.hidden(pooled_output))
+        logits = self.classifier(hidden_logits)
+
+        x = self.sigmoid(logits)
+
+        return x
+
+class NERClassifier_E(nn.Module):
+    def __init__(self):
+        super(NERClassifier_E, self).__init__()
+        self.character_bert = CharacterBertModel.from_pretrained('./character_bert_model/pretrained-models/general_character_bert/')
+        
+    def forward(self, x):
+        # Forward pass through the CharacterBert model
+        outputs = self.character_bert(x)[0]
+        return outputs
+
+class NERClassifier(nets.EncoderHeadNet):
+    def __init__(self,
+                hidden_size: tuple[int, int] = (200, 100),
+                softmax: bool = False):
+        """
+        NERClassifier is a neural network model for Named Entity Recognition (NER) tasks.
+        """
+
+        super(NERClassifier, self).__init__(
+            NERClassifier_E(),
+            NERClassifier_D()
+        )
