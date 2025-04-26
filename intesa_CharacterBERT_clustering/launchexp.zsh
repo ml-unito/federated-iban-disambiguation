1888510b7d6f108bc3384857d5d09f59a5fa1c94
Roberto Esposito <roberto.esposito@unito.it>
2025-04-26 09:24:24 +0200

    Upadates launch scripts to support new algorithms

--- intesa_CharacterBERT_clustering/launchexp.zsh
+++ intesa_CharacterBERT_clustering/launchexp.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
 seed=$1
+exp=config/$2.yaml
+alg=config/$3.yaml
 
 if [ -z "$seed" ]; then
     echo "Usage: $0 <seed>"
@@ -12,12 +14,12 @@ uv run split_dataset.py --seed $seed
 uv run preprocessing.py
 
 echo -e "\033[0;32mUpdating config files...\033[0m"
-sed -i "s/seed: .*/seed: $seed/" config/exp_federated.yaml
+sed -i "s/seed: .*/seed: $seed/" $exp
 
 if [ $? -ne 0 ]; then
-    echo -e "\033[0;31mError updating config/exp_federated.yaml\033[0m"
+    echo -e "\033[0;31mError updating $exp\033[0m"
     exit 1
 fi
 
 echo -e "\033[0;32mStarting experiment...\033[0m"
-uv run fluke federation config/exp_federated.yaml config/alg_frozen.yaml
+uv run fluke federation $exp $alg
