ac6c5352a8f04ad1691859cb1ba787bbceb64fe9
Giulia Luisa Fumiento <giuliafumiento@gmail.com>
2025-06-16 15:57:13 +0200

    Removes duplicate column "label" in couple dataset.

--- intesa_CharacterBERT_clustering/clustering.py
+++ intesa_CharacterBERT_clustering/clustering.py
@@ -448,7 +448,7 @@ def kernel_accounts_disambiguation(seed: int, weights_path: str, dataset_path: s
 
     pairs_df = create_pairs_kernel_clustering(dataset)
     similarity = create_sim_data(pairs_df[['name1', 'name2', 'label']], 7)
-    pairs_df = pd.concat([pairs_df, similarity], axis=1)
+    pairs_df = pd.concat([pairs_df, similarity.drop(columns=["label"])], axis=1)
 
     # load model
     weights = torch.load(weights_path, weights_only=True)["model"]
